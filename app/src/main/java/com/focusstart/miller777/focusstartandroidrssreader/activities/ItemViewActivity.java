7aa965d3b80e03483c5bab2c7447b99b24a00de1
Miller777 <miller777@mail.ru>
2019-06-08 02:45:26 +0300

    Очередная попытка сделать кэширование в WebView: безуспешно;
    Проверяем состояние сети, в зависимости от него назначаем WebView разные режимы кэширования;

--- app/src/main/java/com/focusstart/miller777/focusstartandroidrssreader/activities/ItemViewActivity.java
+++ app/src/main/java/com/focusstart/miller777/focusstartandroidrssreader/activities/ItemViewActivity.java
@@ -1,5 +1,7 @@
 package com.focusstart.miller777.focusstartandroidrssreader.activities;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -32,19 +34,34 @@ public class ItemViewActivity extends AppCompatActivity {
 
 
         webView = findViewById(R.id.item_web_view);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-//        webView.getSettings().setAppCacheEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-
         webView.setWebChromeClient(new WebChromeClient());
+////        Uri data = getIntent().getData();
 
-//        Uri data = getIntent().getData();
+        webView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
+        webView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        webView.getSettings().setAllowFileAccess( true );
+        webView.getSettings().setAppCacheEnabled( true );
+        webView.getSettings().setJavaScriptEnabled( true );
+        webView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT ); // load online by default
+
         Uri data = Uri.parse(getIntent().getStringExtra("LINK"));
+
+        Log.d(TAG, "isNetworkAvailable = " + isNetworkAvailable());
+
+
+        if ( !isNetworkAvailable() ) { // loading offline
+            webView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+
         webView.loadUrl(data.toString());
-        webView.saveWebArchive("test.mht");
-//        webView.loadUrl("test.web");
 
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
